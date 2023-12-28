125500f1fe50a65853728fa1e63371dca98102bb
LilisRisnawatiNurulliyah <LilisRisnawatiNurulliyah>
2023-12-28 21:43:50 +0700

    vue js

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import { createApp } from 'vue'
+import PostsIndex from './components/Posts/Index.vue'
+
+const app = createApp({})
+app.component('posts-index', PostsIndex)
+app.mount('#app')
